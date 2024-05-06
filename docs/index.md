90046bc335be14afe5ad2c75b896e1438646ddda
itskalvik <kjakkala@uncc.edu>
2024-05-06 17:11:24 -0400

    Setup mkdocs

---
+++ docs/index.md
@@ -0,0 +1,2 @@
+::: sgptools.models.continuous_sgp.continuous_sgp
+::: sgptools.models.continuous_sgp.init_osgpr
--- sgptools/models/continuous_sgp.py
+++ sgptools/models/continuous_sgp.py
@@ -22,31 +22,33 @@ from .core.osgpr import OSGPR_VFE
 from .core.augmented_sgpr import AugmentedSGPR
 
 
-'''
-Get sensor placement solution using the Continuous VFE-SGP method
-
-Refer to the following papers for more details:
-Efficient Sensor Placement from Regression with Sparse Gaussian Processes in Continuous and Discrete Spaces [Jakkala and Akella, 2023]
-Multi-Robot Informative Path Planning from Regression with Sparse Gaussian Processes [Jakkala and Akella, 2024]
-
-Args:
-    num_inducing: Number of inducing points
-    X_train: Numpy array (n ,d) with n d-dimensional data points
-    noise_variance: data variance
-    kernel: kernel function
-    transform: (optional) Transform object
-    Xu_init: (optional) Initial inducing points array
-    Xu_time: (optional) Temporal inducing points (used in spatio-temporal models)
-    orientation: If True, a additionl dimension is added to the 
-                 inducing points to represent the FoV orientation
-'''
 def continuous_sgp(num_inducing, X_train, noise_variance, kernel, 
                    transform=None,
                    Xu_init=None, 
                    Xu_time=None, 
                    orientation=False,
                    **kwargs):
+    """Get sensor placement solutions using the Continuous-SGP method
 
+    Refer to the following papers for more details:
+        - Efficient Sensor Placement from Regression with Sparse Gaussian Processes in Continuous and Discrete Spaces [[Jakkala and Akella, 2023](https://www.itskalvik.com/publication/sgp-sp/)]
+        - Multi-Robot Informative Path Planning from Regression with Sparse Gaussian Processes [[Jakkala and Akella, 2024](https://www.itskalvik.com/publication/sgp-ipp/)]
+
+    Args:
+        num_inducing (int): Number of inducing points
+        X_train (ndarray): (n, d); Unlabeled random sampled training points
+        noise_variance (float): data variance
+        kernel (gpflow.kernels.Kernel): gpflow kernel function
+        transform (Transform): Transform object
+        Xu_init (ndarray): (m, d); Initial inducing points
+        Xu_time (ndarray): (t, d); Temporal inducing points used in spatio-temporal models
+        orientation (bool): If True, a additionl dimension is added to the 
+                            inducing points to represent the FoV orientation
+
+    Returns:
+        sgpr (AugmentedSGPR): Optimized sparse Gaussian process model
+        loss (ndarray): Loss values computed during training
+    """
     # Generate init inducing points
     if Xu_init is None:
         Xu_init = get_inducing_pts(X_train, num_inducing, 
@@ -67,25 +69,29 @@ def continuous_sgp(num_inducing, X_train, noise_variance, kernel,
 
     return sgpr, loss
 
-'''
-Initialize a VFE OSGPR model with an RBF kernel with 
-unit variance and lengthcales, and 0.001 noise variance.
-Used in the Online Continuous SGP approach. 
 
-Args:
-    X_train: Numpy array (n ,d) with n d-dimensional data points. 
-             They only effect the initial inducing point locations, 
-             i.e., limits them to the bounds of the data
-    num_inducing: int, Number of inducing points
-    lengthscales: float or list of floats for each dimension of the data. Kernel lengthscale
-    variance: float, Kernel variance
-    noise_variance: float, Data noise variance
-'''
 def init_osgpr(X_train, 
                num_inducing=10, 
                lengthscales=1.0, 
                variance=1.0,
                noise_variance=0.001):
+    """Initialize a VFE OSGPR model with an RBF kernel with 
+    unit variance and lengthcales, and 0.001 noise variance.
+    Used in the Online Continuous SGP approach. 
+
+    Args:
+        X_train (ndarray): (n, d); Unlabeled random sampled training points. 
+                        They only effect the initial inducing point locations, 
+                        i.e., limits them to the bounds of the data
+        num_inducing (int): Number of inducing points
+        lengthscales (ndarray or list): Kernel lengthscale of each dimension of the data
+        variance (float): Kernel variance
+        noise_variance (float): Data variance
+
+    Returns:
+        online_param (OSGPR_VFE): Initialized online sparse Gaussian process model
+    """
+
     y_train = np.zeros((len(X_train), 1), dtype=X_train.dtype)
     Z_init = get_inducing_pts(X_train, num_inducing)
     init_param = gpflow.models.SGPR((X_train, y_train),
