091ee08a2f9a565451851ff26781ca3f55194f96
itskalvik <kjakkala@uncc.edu>
2024-05-08 18:21:26 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
     version=__version__,
     packages=find_packages(),
     python_requires='>=3.6',
-    url='https://github.com/itskalvik/sgp-tools',
+    url='https://www.itskalvik.com/sgp-tools',
     license='Apache-2.0',
+    author_email='itskalvik@gmail.com',
+    author='Kalvik Jakkala',
+    description='Software suite for Sensor Placement and Informative Path Planning',
 )
