7f76b64a62a7def1c6183ae53253e5228bae65a5
itskalvik <itskalvik@gmail.com>
2025-07-15 13:21:37 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
--- sgptools/__init__.py
+++ sgptools/__init__.py
@@ -11,7 +11,7 @@ The library includes python code for the following:
 
 """
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 __author__ = 'Kalvik'
 
 from .core import *
